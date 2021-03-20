c6e25d94301fb7f3e010633c2874b8f7a12533bf
Daniel Bassani <17dab3@queensu.ca>
2021-03-20 14:51:30 -0400

    all the UI is here

--- Assets/Scripts/UIFunctions.cs
+++ Assets/Scripts/UIFunctions.cs
@@ -3,23 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 using Mirror;
 
 public class UIFunctions : MonoBehaviour
 {
+    //Game audio
+    public AudioSource source;
+    public AudioSource buttonSource;
+    public AudioClip menuSelect;
+    public AudioClip menuBack;
+
+    //PC related UI
     public GameObject mainMenuPanel;
     public GameObject gameSelectPanel;
+    public GameObject settingsPanel;
+
+    //Mobile related UI
     public GameObject mobileMainMenuPanel;
     public GameObject mobileConnectedPanel;
     public GameObject mobileSettingsPanel;
-    public GameObject network;
     public Slider xSens;
     public Slider ySens;
     public Slider zSens;
 
+    //Network reference?
+    public GameObject network;
+    
+
     public void Start()
     {
-        /*
         if(SystemInfo.deviceType == DeviceType.Handheld)
         {
             Debug.Log("handheld");
@@ -30,12 +43,12 @@ public class UIFunctions : MonoBehaviour
             Debug.Log("PC");
             mainMenuPanel.SetActive(true);
         }
-        */
     }
     
     public void PressPlay(){
         mainMenuPanel.SetActive(false);
         gameSelectPanel.SetActive(true);
+        buttonSource.PlayOneShot(menuSelect);
     }
 
     public void MobilePressPlay()
@@ -55,9 +68,24 @@ public class UIFunctions : MonoBehaviour
         mobileSettingsPanel.SetActive(true);
     }
 
+    public void PressSettings()
+    {
+        mainMenuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+        buttonSource.PlayOneShot(menuSelect);
+    }
+
+    public void SettingsBack()
+    {
+        settingsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+        buttonSource.PlayOneShot(menuBack);
+    }
+
     public void GameSelectBack(){
         gameSelectPanel.SetActive(false);
         mainMenuPanel.SetActive(true);
+        buttonSource.PlayOneShot(menuBack);
     }
 
     public void MobileSettingsBack()
@@ -67,11 +95,13 @@ public class UIFunctions : MonoBehaviour
     }
 
     public void Quit(){
+        buttonSource.PlayOneShot(menuSelect);
         Application.Quit();
     }
 
     public void LoadGame(int sceneNumber)
     {
+        buttonSource.PlayOneShot(menuSelect);
         SceneManager.LoadScene(sceneNumber);
     }
 
@@ -80,5 +110,20 @@ public class UIFunctions : MonoBehaviour
         xSens.value = 0.5f;
         ySens.value = 0.5f;
         zSens.value = 0.5f;
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        source.volume = volume;
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        buttonSource.volume = volume;
+    }
+
+    public void SetQuality(int index)
+    {
+        QualitySettings.SetQualityLevel(index);
     }
 }
