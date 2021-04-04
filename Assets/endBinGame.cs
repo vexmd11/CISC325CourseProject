b8c466cbfa44baa972011ee1519255c63c854269
DanielBallem <33844493+DanielBallem@users.noreply.github.com>
2021-04-04 11:32:23 -0400

    boots back from second game

---
+++ Assets/endBinGame.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class endBinGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    spawnerScript spawner;
+    void Start()
+    {
+        spawner = this.GetComponent<spawnerScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawner.score >=25){
+            SceneManager.LoadScene(0);
+        }
+    }
+}
